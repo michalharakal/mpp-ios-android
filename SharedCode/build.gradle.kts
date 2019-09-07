375fd0a6bf6414c242d35dbbb11322bde8bc8d17
Michal Harakal <michal.harakal@telekom.de>
2019-09-07 15:56:51 +0200

    Use cocoapods plugin for consuming SharedLib with iOS

--- SharedCode/build.gradle.kts
+++ SharedCode/build.gradle.kts
@@ -2,7 +2,10 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform")
+    id("org.jetbrains.kotlin.native.cocoapods")
 }
+
+version = "1.0"
 
 kotlin {
     //select iOS target platform depending on the Xcode environment variables
@@ -14,9 +17,7 @@ kotlin {
 
     iOSTarget("ios") {
         binaries {
-            framework {
-                baseName = "SharedCode"
-            }
+            framework ("SharedLib")
         }
     }
 
@@ -29,28 +30,10 @@ kotlin {
     sourceSets["androidMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
-}
 
-
-val packForXcode by tasks.creating(Sync::class) {
-    group = "build"
-
-    //selecting the right configuration for the iOS framework depending on the Xcode environment variables
-    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
-
-    inputs.property("mode", mode)
-    dependsOn(framework.linkTask)
-
-    val targetDir = File(buildDir, "xcode-frameworks")
-    from({ framework.outputDirectory })
-    into(targetDir)
-
-    doLast {
-        val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText("#!/bin/bash\nexport 'JAVA_HOME=${System.getProperty("java.home")}'\ncd '${rootProject.rootDir}'\n./gradlew \$@\n")
-        gradlew.setExecutable(true)
+    cocoapods {
+        // Configure fields required by CocoaPods.
+        summary = "Shared library"
+        homepage = "https://github.com/touchlab/DroidconKotlin"
     }
 }
-
-tasks.getByName("build").dependsOn(packForXcode)
